2b8974e66b7f6837b9f73ce258697cada8d9d0e4
Martin <mkk@webkenth.dk>
2023-10-29 19:04:54 +0100

    added migration dataset

--- database/migrations/2023_10_29_170248_create_registrations_table.php
+++ database/migrations/2023_10_29_170248_create_registrations_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('registrations', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('hours');
+            $table->integer('rate')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('project_id')->constrained();
             $table->timestamps();
         });
     }
