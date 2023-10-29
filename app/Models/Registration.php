9740b6909707af293e9bc62c7212fca2c61077e4
Martin <mkk@webkenth.dk>
2023-10-29 18:58:17 +0100

    initial project

---
+++ app/Models/Registration.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Registration extends Model
+{
+    use HasFactory;
+
+    public function project()
+    {
+        // TODO: Implement project() method.
+        // Figure out which method to use to correctly define the relationship
+        // https://laravel.com/docs/10.x/eloquent-relationships#one-to-many
+    }
+}
